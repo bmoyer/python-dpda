cb88f28f48c8b54dd16889e779843fb5e1894861
Ben Moyer <bmoyer2@pride.hofstra.edu>
2014-06-28 17:13:55 -0400

    Restructured State flow

--- State.py
+++ State.py
@@ -4,26 +4,32 @@ class State(object):
     def __init__(self,state_type="Default", next_state=None):
         self.state_type = state_type
         self.transitions  = set()
-        if state_type == "Start":
-            self.next_state == next_state
-        
+        self.next_state = next_state
 
-    def add_transition(self, character, next_state):
+    def add_transition(self, character=None, next_state):
         transition = {'char': character, 'next': next_state }
         self.transitions.add(transition)
 
-    def get_next_state(self):
+    def get_next_state(self, character=None):
         if self.state_type == "Start":
-            return self.next_state
+            (next_state, ) = self.transitions
+            return next_state
 
         if self.state_type == "Default":
             for transition in self.transitions:
+                if transition['char'] == character:
+                    return transition['next']
+
+        if self.state_type == "Push":
+            return self.next_state
+                  
+        if self.state_type = "Pop":
+
+
                 
 
     def action(self, tape, stack):
          if self.state_type == "Default":
-             self.tape = tape
-             self.stack = stack
-             self.get_next_state().action(tape, stack)
+             self.get_next_state(character=tape[0]).action(tape, stack)
          if self.state_type == "Push":
-            
+             stack.append(
