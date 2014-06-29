b2ff5b03e1997865d456525f2b2f3888345b2f0b
Ben Moyer <bmoyer2@pride.hofstra.edu>
2014-06-29 14:33:09 -0400

    lots of comments

--- PDA.py
+++ PDA.py
@@ -1,6 +1,9 @@
 class PDA(object):
     def __init__(self, start_state, tape=""):
+        """Automata are created by supplying an initial state."""
         self.start_state = start_state
 
     def start(self, tape, stack=[]):
+        """After we have the tape, stack, and the inital state, we simply call
+        the action() function of the initial state."""
         self.start_state.action(tape, stack)
--- State.py
+++ State.py
@@ -1,14 +1,20 @@
 class State(object):
     def __init__(self, state_type, next_state=None):
+        """If state_type='Start', then a next_state must be provided."""
         self.state_type = state_type
         self.transitions = list()
         self.next_state = next_state
 
     def add_transition(self, next_state, character=None):
+        """Adds a transition to the list of the state's acceptable
+        transitions.  A transition consists of at least a  next state, and a
+        character depending on the type of the state."""
         transition = {'char': character, 'next': next_state}
         self.transitions.append(transition)
 
     def get_next_state(self, character=None):
+        """Retrieves the next state that the PDA should go to, based on the
+        current state's type, and an input from either the tape or stack."""
         if self.state_type == "Start":
             return self.next_state
 
@@ -29,6 +35,11 @@ class State(object):
                     return transition['next']
 
     def action(self, tape, stack):
+        """If state_type is Read, Push, or Pop, the respective action is taken
+        and the get_next_state() retrieves the next state in the PDA.  The
+        action() function of the next state is then called.
+        If state_type is Start, next state's action() function is called.
+        If state_type is Accept, the state simply prints a message."""
         try:
             if self.state_type == "Start":
                 self.get_next_state().action(tape, stack)
